7a98854b934bb36eea3848703be3a28d74733d07
surya <sv00483947@techmahindra.com>
2019-02-06 12:40:16 +0530

    upload tested

--- components/manufacturer.js
+++ components/manufacturer.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const router = express.Router();
+
+// default options
+// Max size one mb
+router.use(fileUpload({
+  limits: { fileSize: 1 * 1024 * 1024 },
+  createParentPath: true,
+  safeFileNames: /\\/g,
+  preserveExtension: true
+}));
+
 const { ensureAuthenticated } = require('../config/auth');
 
-router.get('/create-request', ensureAuthenticated, (req, res) =>
+router.get('/create_request', ensureAuthenticated, (req, res, next) =>
   res.render('forms/manufacturer-request-form', { layout: 'layouts/dashboard-layout', user: req.user })
 );
+
+router.post('/create_request', ensureAuthenticated, (req, res, next) =>{
+  console.log(req.body);
+  console.log(req.user);
+  if (Object.keys(req.files).length != 0) {
+    let file = req.files.manufacturer_attachments;
+    console.log(file);    
+    file.mv('uploads/'+file.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+    });
+  }
+
+  res.send('ok');
+});
 
 module.exports = router;
