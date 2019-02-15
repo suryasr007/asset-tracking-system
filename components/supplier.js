d687c662787d81a1814ab70134bd56bc472f3db0
Surya Teja Reddy Valluri <suryav@juniper.net>
2019-02-15 17:45:09 +0530

    Active requests completed

--- components/supplier.js
+++ components/supplier.js
@@ -11,7 +11,7 @@ const { ensureAuthenticated } = require("../config/auth");
 router.get('/active_requests', ensureAuthenticated, (req, res, next)=>{
     // console.log(req);
     // res.send('ok');
-    Part.find({supplier_email:req.user.email})
+    Part.find({supplier_email:req.user.email, supplier_acceptance:'pending'})
     .then((records)=>{
         res.render("view-requests/supplier", {
             layout: "layouts/dashboard-layout",
@@ -20,6 +20,19 @@ router.get('/active_requests', ensureAuthenticated, (req, res, next)=>{
         })
     })
 })
+
+router.get('/:record/:status', ensureAuthenticated, (req, res, next)=>{
+
+    Part.findByIdAndUpdate(req.params.record, {supplier_acceptance: req.params.status})
+        .then((record)=>{
+            res.redirect('/supplier/active_requests');
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.redirect(500);
+        });
+});
+
 
 router.get('/download/:filename', ensureAuthenticated, (req, res, next)=>{
     console.log('File Name:'+req.params.filename);
